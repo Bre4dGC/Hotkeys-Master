2e785c3455e41246100f237e16d60ee45523cb3d
Bread <glebbreadc@gmail.com>
2024-06-24 19:09:11 +0300

    added control panel and side navigation bar

---
+++ HotKeys Master/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace HotKeys_Master
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            guna2Button3 = new Guna.UI2.WinForms.Guna2Button();
+            guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            SidebarPanel = new FlowLayoutPanel();
+            HomePanel = new Panel();
+            LeftPanel1 = new Panel();
+            SettingsPanel = new Panel();
+            LeftPanel2 = new Panel();
+            AboutPanel = new Panel();
+            LeftPanel3 = new Panel();
+            MainContent = new Panel();
+            guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(components);
+            ControlPanel = new Panel();
+            MinimizeButton = new Button();
+            MaximizeButton = new Button();
+            CloseButton = new Button();
+            notifyIcon1 = new NotifyIcon(components);
+            SidebarPanel.SuspendLayout();
+            HomePanel.SuspendLayout();
+            SettingsPanel.SuspendLayout();
+            AboutPanel.SuspendLayout();
+            ControlPanel.SuspendLayout();
+            SuspendLayout();
+            // 
+            // guna2Button2
+            // 
+            guna2Button2.Animated = true;
+            guna2Button2.BackColor = Color.White;
+            guna2Button2.CheckedState.FillColor = SystemColors.Control;
+            guna2Button2.Cursor = Cursors.Hand;
+            guna2Button2.CustomizableEdges = customizableEdges1;
+            guna2Button2.DisabledState.BorderColor = Color.DarkGray;
+            guna2Button2.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2Button2.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2Button2.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            resources.ApplyResources(guna2Button2, "guna2Button2");
+            guna2Button2.FillColor = Color.White;
+            guna2Button2.ForeColor = Color.FromArgb(75, 75, 75);
+            guna2Button2.HoverState.FillColor = Color.FromArgb(241, 241, 241);
+            guna2Button2.Image = (Image)resources.GetObject("guna2Button2.Image");
+            guna2Button2.ImageAlign = HorizontalAlignment.Left;
+            guna2Button2.ImageSize = new Size(45, 45);
+            guna2Button2.Name = "guna2Button2";
+            guna2Button2.PressedColor = Color.DarkGray;
+            guna2Button2.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            guna2Button2.Click += guna2Button2_Click;
+            // 
+            // guna2Button3
+            // 
+            guna2Button3.Animated = true;
+            guna2Button3.BackColor = Color.White;
+            guna2Button3.CheckedState.FillColor = SystemColors.Control;
+            guna2Button3.Cursor = Cursors.Hand;
+            guna2Button3.CustomizableEdges = customizableEdges3;
+            guna2Button3.DisabledState.BorderColor = Color.DarkGray;
+            guna2Button3.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2Button3.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2Button3.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            resources.ApplyResources(guna2Button3, "guna2Button3");
+            guna2Button3.FillColor = Color.White;
+            guna2Button3.ForeColor = Color.FromArgb(75, 75, 75);
+            guna2Button3.HoverState.FillColor = Color.FromArgb(241, 241, 241);
+            guna2Button3.Image = (Image)resources.GetObject("guna2Button3.Image");
+            guna2Button3.ImageAlign = HorizontalAlignment.Left;
+            guna2Button3.ImageSize = new Size(45, 45);
+            guna2Button3.Name = "guna2Button3";
+            guna2Button3.PressedColor = Color.DarkGray;
+            guna2Button3.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            guna2Button3.Click += guna2Button3_Click;
+            // 
+            // guna2Button1
+            // 
+            guna2Button1.Animated = true;
+            guna2Button1.BackColor = Color.White;
+            guna2Button1.CheckedState.FillColor = SystemColors.Control;
+            guna2Button1.Cursor = Cursors.Hand;
+            guna2Button1.CustomizableEdges = customizableEdges5;
+            guna2Button1.DisabledState.BorderColor = Color.DarkGray;
+            guna2Button1.DisabledState.CustomBorderColor = Color.DarkGray;
+            guna2Button1.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            guna2Button1.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            resources.ApplyResources(guna2Button1, "guna2Button1");
+            guna2Button1.FillColor = Color.White;
+            guna2Button1.ForeColor = Color.FromArgb(75, 75, 75);
+            guna2Button1.HoverState.FillColor = Color.FromArgb(241, 241, 241);
+            guna2Button1.Image = (Image)resources.GetObject("guna2Button1.Image");
+            guna2Button1.ImageAlign = HorizontalAlignment.Left;
+            guna2Button1.ImageSize = new Size(45, 45);
+            guna2Button1.Name = "guna2Button1";
+            guna2Button1.PressedColor = Color.DarkGray;
+            guna2Button1.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            guna2Button1.Click += guna2Button1_Click;
+            // 
+            // SidebarPanel
+            // 
+            SidebarPanel.BackColor = Color.White;
+            SidebarPanel.Controls.Add(HomePanel);
+            SidebarPanel.Controls.Add(SettingsPanel);
+            SidebarPanel.Controls.Add(AboutPanel);
+            resources.ApplyResources(SidebarPanel, "SidebarPanel");
+            SidebarPanel.Name = "SidebarPanel";
+            // 
+            // HomePanel
+            // 
+            HomePanel.Controls.Add(guna2Button1);
+            HomePanel.Controls.Add(LeftPanel1);
+            resources.ApplyResources(HomePanel, "HomePanel");
+            HomePanel.Name = "HomePanel";
+            // 
+            // LeftPanel1
+            // 
+            LeftPanel1.BackColor = SystemColors.ActiveCaption;
+            resources.ApplyResources(LeftPanel1, "LeftPanel1");
+            LeftPanel1.Name = "LeftPanel1";
+            // 
+            // SettingsPanel
+            // 
+            SettingsPanel.Controls.Add(guna2Button2);
+            SettingsPanel.Controls.Add(LeftPanel2);
+            resources.ApplyResources(SettingsPanel, "SettingsPanel");
+            SettingsPanel.Name = "SettingsPanel";
+            // 
+            // LeftPanel2
+            // 
+            LeftPanel2.BackColor = SystemColors.ActiveCaption;
+            resources.ApplyResources(LeftPanel2, "LeftPanel2");
+            LeftPanel2.Name = "LeftPanel2";
+            // 
+            // AboutPanel
+            // 
+            AboutPanel.Controls.Add(guna2Button3);
+            AboutPanel.Controls.Add(LeftPanel3);
+            resources.ApplyResources(AboutPanel, "AboutPanel");
+            AboutPanel.Name = "AboutPanel";
+            // 
+            // LeftPanel3
+            // 
+            LeftPanel3.BackColor = SystemColors.ActiveCaption;
+            resources.ApplyResources(LeftPanel3, "LeftPanel3");
+            LeftPanel3.Name = "LeftPanel3";
+            // 
+            // MainContent
+            // 
+            resources.ApplyResources(MainContent, "MainContent");
+            MainContent.Name = "MainContent";
+            // 
+            // guna2BorderlessForm1
+            // 
+            guna2BorderlessForm1.BorderRadius = 30;
+            guna2BorderlessForm1.ContainerControl = this;
+            guna2BorderlessForm1.DockIndicatorTransparencyValue = 0.6D;
+            guna2BorderlessForm1.TransparentWhileDrag = true;
+            // 
+            // ControlPanel
+            // 
+            ControlPanel.BackColor = Color.White;
+            ControlPanel.Controls.Add(MinimizeButton);
+            ControlPanel.Controls.Add(MaximizeButton);
+            ControlPanel.Controls.Add(CloseButton);
+            resources.ApplyResources(ControlPanel, "ControlPanel");
+            ControlPanel.Name = "ControlPanel";
+            ControlPanel.MouseDoubleClick += ControlPanel_MouseDoubleClick;
+            ControlPanel.MouseDown += ControlPanel_MouseDown;
+            ControlPanel.MouseMove += ControlPanel_MouseMove;
+            ControlPanel.MouseUp += ControlPanel_MouseUp;
+            // 
+            // MinimizeButton
+            // 
+            resources.ApplyResources(MinimizeButton, "MinimizeButton");
+            MinimizeButton.Cursor = Cursors.Hand;
+            MinimizeButton.FlatAppearance.BorderSize = 0;
+            MinimizeButton.Name = "MinimizeButton";
+            MinimizeButton.UseVisualStyleBackColor = true;
+            MinimizeButton.Click += MinimizeButton_Click;
+            // 
+            // MaximizeButton
+            // 
+            resources.ApplyResources(MaximizeButton, "MaximizeButton");
+            MaximizeButton.Cursor = Cursors.Hand;
+            MaximizeButton.FlatAppearance.BorderSize = 0;
+            MaximizeButton.Name = "MaximizeButton";
+            MaximizeButton.UseVisualStyleBackColor = true;
+            MaximizeButton.Click += MaximizeButton_Click;
+            // 
+            // CloseButton
+            // 
+            resources.ApplyResources(CloseButton, "CloseButton");
+            CloseButton.Cursor = Cursors.Hand;
+            CloseButton.FlatAppearance.BorderSize = 0;
+            CloseButton.Name = "CloseButton";
+            CloseButton.UseVisualStyleBackColor = true;
+            CloseButton.Click += CloseButton_Click;
+            // 
+            // notifyIcon1
+            // 
+            resources.ApplyResources(notifyIcon1, "notifyIcon1");
+            notifyIcon1.MouseDoubleClick += notifyIcon1_MouseDoubleClick;
+            // 
+            // MainForm
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.WhiteSmoke;
+            Controls.Add(MainContent);
+            Controls.Add(SidebarPanel);
+            Controls.Add(ControlPanel);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "MainForm";
+            Load += MainForm_Load;
+            SidebarPanel.ResumeLayout(false);
+            HomePanel.ResumeLayout(false);
+            SettingsPanel.ResumeLayout(false);
+            AboutPanel.ResumeLayout(false);
+            ControlPanel.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private FlowLayoutPanel SidebarPanel;
+        private Panel MainContent;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private Guna.UI2.WinForms.Guna2Button guna2Button3;
+        private Panel LeftPanel1;
+        private Panel LeftPanel2;
+        private Panel LeftPanel3;
+        private Panel HomePanel;
+        private Panel SettingsPanel;
+        private Panel AboutPanel;
+        private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
+        private Panel ControlPanel;
+        private Button CloseButton;
+        private Button MaximizeButton;
+        private Button MinimizeButton;
+        private NotifyIcon notifyIcon1;
+    }
+}
---
+++ HotKeys Master/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System.Diagnostics;
+using System.Windows.Forms;
+using Gma.System.MouseKeyHook;
+using Guna.UI2.WinForms;
+
+namespace HotKeys_Master
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        bool Dragging = false;
+        Point DragCursorPoint;
+        Point DragFormPoint;
+
+        private void PanelStyleReset()
+        {
+            LeftPanel1.Visible = false;
+            LeftPanel2.Visible = false;
+            LeftPanel3.Visible = false;
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            PanelStyleReset();
+            LeftPanel1.Visible = true;
+        }
+
+        private void guna2Button2_Click(object sender, EventArgs e)
+        {
+            PanelStyleReset();
+            LeftPanel2.Visible = true;
+        }
+
+        private void guna2Button3_Click(object sender, EventArgs e)
+        {
+            PanelStyleReset();
+            LeftPanel3.Visible = true;
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void MaximizeButton_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Normal)
+            {
+                this.WindowState = FormWindowState.Maximized;
+            }
+            else
+                this.WindowState = FormWindowState.Normal;
+        }
+
+        private void MinimizeButton_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void ControlPanel_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Normal)
+            {
+                this.WindowState = FormWindowState.Maximized;
+            }
+            else
+                this.WindowState = FormWindowState.Normal;
+        }
+
+        private void ControlPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            Dragging = true;
+            DragCursorPoint = Cursor.Position;
+            DragFormPoint = this.Location;
+        }
+
+        private void ControlPanel_MouseUp(object sender, MouseEventArgs e)
+        {
+            Dragging = false;
+        }
+
+        private void ControlPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (Dragging)
+            {
+                Point dif = Point.Subtract(Cursor.Position, new Size(DragCursorPoint));
+                this.Location = Point.Add(DragFormPoint, new Size(dif));
+            }
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            notifyIcon1.BalloonTipTitle = "Hotkey Master";
+            notifyIcon1.BalloonTipText = "The application has switched to background mode";
+            notifyIcon1.Text = "Hotkey Master";
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            notifyIcon1.Visible = false;
+            WindowState = FormWindowState.Normal;
+        }
+    }
+}
