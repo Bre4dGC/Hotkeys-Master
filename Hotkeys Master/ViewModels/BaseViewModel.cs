93921b41cf2abfe93141b2cc920218fee6cf45f4
Bre4dGC <glebbreadc@gmail.com>
2024-11-17 23:36:34 +0300

    Created ViewModels

---
+++ Hotkeys Master/ViewModels/BaseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Hotkeys_Master.ViewModels
+{
+    internal class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
