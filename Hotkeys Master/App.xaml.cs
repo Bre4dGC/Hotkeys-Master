ba628798e1ede9da3bfc9082de81a786c8760136
Bre4dGC <glebbreadc@gmail.com>
2024-11-17 23:38:39 +0300

    Rewrites to WPF

---
+++ Hotkeys Master/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Hotkeys_Master.Views.Windows;
+using System.Windows;
+
+namespace Hotkeys_Master
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = new MainWindow();
+            MainWindow.Show();
+
+            base.OnStartup(e);
+        }
+    }
+}
