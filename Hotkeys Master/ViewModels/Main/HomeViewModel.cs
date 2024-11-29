7b34163d4f123cb7eb59384d1dd5a9bb3b2aeb6e
Bre4dGC <glebbreadc@gmail.com>
2024-11-29 23:15:50 +0300

    Created hotkeys collection and edit, remove command properties

--- Hotkeys Master/ViewModels/Main/HomeViewModel.cs
+++ Hotkeys Master/ViewModels/Main/HomeViewModel.cs
@@ -1,12 +1,46 @@
-﻿using System.Windows.Input;
+﻿using CommunityToolkit.Mvvm.Input;
+using Hotkeys_Master.Models;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace Hotkeys_Master.ViewModels.Main
 {
     class HomeViewModel : BaseViewModel
     {
-        public ICommand AddHotkeyCommand { get; set; }
+        public Hotkey Hotkey { get; set; }
+        public ObservableCollection<Hotkey> Hotkeys { get; }
+
+        public ICommand AddHotkeyCommand { get; }
+        public ICommand EditHotkeyCommand { get; }
+        public ICommand RemoveHotkeyCommand { get; }
 
         public HomeViewModel()
+        {
+            Hotkeys = new ObservableCollection<Hotkey>();
+
+            AddHotkeyCommand = new RelayCommand(AddHotkey);
+            EditHotkeyCommand = new RelayCommand(EditHotkey);
+            RemoveHotkeyCommand = new RelayCommand(RemoveHotkey);
+
+            LoadHotkeys();
+        }
+
+        private void AddHotkey()
+        {
+
+        }
+
+        private void EditHotkey()
+        {
+
+        }
+
+        private void RemoveHotkey()
+        {
+
+        }
+
+        private void LoadHotkeys()
         {
 
         }
