1584df7aa02a0f5e3ef6e768e39fddf826700e6b
Bre4dGC <glebbreadc@gmail.com>
2024-11-29 23:06:21 +0300

    Created hotkey status

--- Hotkeys Master/Models/Hotkey.cs
+++ Hotkeys Master/Models/Hotkey.cs
@@ -5,8 +5,16 @@ namespace Hotkeys_Master.Models
     internal class Hotkey
     {
         public Guid Id { get; set; }
-        public string Title { get; set; }
-        public string Path { get; set; }
-        public ICollection<Key> Keys { get; set; }
+        public string? Title { get; set; }
+        public string Path { get; set; } = string.Empty;
+        public Status Status { get; set; } = Status.Created;
+        public ICollection<Key> Keys { get; set; } = new List<Key>();
+    }
+
+    public enum Status
+    {
+        Created,
+        Deleted,
+        Edited
     }
 }
