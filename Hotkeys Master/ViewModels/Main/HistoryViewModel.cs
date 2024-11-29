edce044c675419d1d913cabecb74a22af7281d89
Bre4dGC <glebbreadc@gmail.com>
2024-11-29 23:37:09 +0300

    Created history collection and clear command

--- Hotkeys Master/ViewModels/Main/HistoryViewModel.cs
+++ Hotkeys Master/ViewModels/Main/HistoryViewModel.cs
@@ -1,6 +1,28 @@
-﻿namespace Hotkeys_Master.ViewModels.Main
+﻿using CommunityToolkit.Mvvm.Input;
+using Hotkeys_Master.Models;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace Hotkeys_Master.ViewModels.Main
 {
     class HistoryViewModel : BaseViewModel
     {
+        public Hotkey Hotkey { get; set; }
+        public ObservableCollection<Hotkey> Hotkeys { get; }
+
+        public ICommand ClearHistoryCommand { get; }
+
+        public HistoryViewModel(Hotkey hotkey)
+        {
+            Hotkey = hotkey;
+            Hotkeys = new ObservableCollection<Hotkey>();
+
+            ClearHistoryCommand = new RelayCommand(ClearHistory);
+        }
+
+        private void ClearHistory()
+        {
+            Hotkeys.Clear();
+        }
     }
 }
