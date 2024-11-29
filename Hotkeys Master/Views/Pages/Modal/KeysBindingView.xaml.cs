fc8faa9ec64588d6a33672073aad753000e1b066
Bre4dGC <glebbreadc@gmail.com>
2024-11-29 23:43:50 +0300

    Added create hotkey pages

---
+++ Hotkeys Master/Views/Pages/Modal/KeysBindingView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Hotkeys_Master.Views.Pages.Modal
+{
+    /// <summary>
+    /// Логика взаимодействия для KeysBindingView.xaml
+    /// </summary>
+    public partial class KeysBindingView : Page
+    {
+        public KeysBindingView()
+        {
+            InitializeComponent();
+        }
+    }
+}
